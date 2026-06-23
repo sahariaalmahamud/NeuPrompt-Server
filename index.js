286f872c5e10f42c4857d1c574dfcb35734e7903
mernstackdevelopersams <mernstackdevelopersams@gmail.com>
2026-06-24 01:37:50 +0600

    installed express, packeges and post the prompts data

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const cors = require("cors");
+const {
+    MongoClient,
+    ServerApiVersion,
+} = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function server() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db("neuprompt_db");
+        const promptsCollection = database.collection("prompts");
+
+        app.post('/api/prompts', async (req, res) => {
+            const prompt = req.body;
+            const result = await promptsCollection.insertOne(prompt);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+server().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
